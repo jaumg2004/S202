3102c43970f0472934dba8a808ee61d9e427f902
João Gabriel Ferreira Ribeiro <151750450+jaumg2004@users.noreply.github.com>
2024-09-02 09:23:59 -0300

    Add files via upload

---
+++ Relatório 4/Main.py
@@ -0,0 +1,28 @@
+from database import Database
+from writeAJson import writeAJson
+
+db = Database(database="mercado", collection="produtos")
+#db.resetDatabase()
+
+#Média de gasto por cliente
+db.collection.aggregate([
+    {"$unwind": "$produtos"},
+    {"$group": {"_id": "$cliente_id", "total": {"$sum": {"$multiply": ["$produtos.quantidade", "$produtos.preco"]}}}},
+    {"$group": {"_id": None, "media": {"$avg": "$total"}}}
+])
+
+#Produto mais vendido
+db.collection.aggregate([
+    {"$unwind": "$produtos"},
+    {"$group": {"_id": "$produtos.descricao", "total": {"$sum": "$produtos.quantidade"}}},
+    {"$sort": {"total": -1}},
+    {"$limit": 1}
+])
+
+#Cliente que mais comprou em cada dia
+db.collection.aggregate([
+    {"$unwind": "$produtos"},
+    {"$group": {"_id": {"cliente": "$cliente_id", "data": "$data_compra"}, "total": {"$sum": {"$multiply": ["$produtos.quantidade", "$produtos.preco"]}}}},
+    {"$sort": {"_id.data": 1, "total": -1}},
+    {"$group": {"_id": "$_id.data", "cliente": {"$first": "$_id.cliente"}, "total": {"$first": "$total"}}}
+])
